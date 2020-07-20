88fc47fab1438cdbce70ba100f4cba633e82c376
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2020-07-20 11:27:09 +0200

    plop

--- test.php
+++ test.php
@@ -8,4 +8,25 @@ if (file_exists(__DIR__ . '/.env')) {
     $dotenv->load();
 }
 
-echo $_ENV["MESSAGE"];
+$host = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASSWORD'];        
+
+$bdd = new \PDO(
+    "mysql:host=".$host.";dbname=".$dbname.";charset=utf8", 
+    $user, 
+    $pass
+);
+
+$articles = $bdd->prepare('SELECT * FROM articles');
+$articles->execute(array());
+$articles = $articles->fetchAll();
+
+foreach($articles as $article): 
+?>
+
+<h1><?= $article['title']; ?></h1>
+<p><?= $article['content']; ?></p>
+
+<?php endforeach; ?>
